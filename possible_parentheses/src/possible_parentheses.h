4d990a62ed7b2d85fa9ed7719e609fe3321b20d2
Nimzerz <aminaimi14@gmail.com>
2019-11-15 18:13:59 -0800

    tested first attempt

--- possible_parentheses/src/possible_parentheses.h
+++ possible_parentheses/src/possible_parentheses.h
@@ -1,20 +1,11 @@
 #include <list>
 
 #define MAX_SIZE 100
-// n: size of the problem
-// result: a list of string, each string represents a case of valid parentheses of size n
-void possibleParenthesis(int n, std::list<std::string> & result)
-{
-    // homework
-    if (n > 0) {
-        possibleParenthesis(0, n, 0, 0, result);
-    }
-}
 
 void possibleParenthesis(int pos, int n, int open, int closed, std::list<std::string> & result) {
     static char str[MAX_SIZE];
     if (closed == n) {
-        result.insert(str);
+        result.push_back(str);
         return;
     }
     if (open < n) {
@@ -24,6 +15,15 @@ void possibleParenthesis(int pos, int n, int open, int closed, std::list<std::st
     if (open > closed) {
         str[pos] = '}';
         possibleParenthesis(pos + 1, n, open, closed + 1, result);
+    }
+}
+
+// n: size of the problem
+// result: a list of string, each string represents a case of valid parentheses of size n
+void possibleParenthesis(int n, std::list<std::string> & result)
+{
+    if (n > 0) {
+        possibleParenthesis(0, n, 0, 0, result);
     }
 }
 
--- possible_parentheses/test/unit_tests.cpp
+++ possible_parentheses/test/unit_tests.cpp
@@ -10,21 +10,42 @@ void showResult(std::list<std::string> & result) {
 TEST(test_recursive, 1) {
     std::list<std::string> result;
     possibleParenthesis(1, result);
-
+    ASSERT_EQ(result.size(), 1);
+    result.remove("{}");
+    ASSERT_EQ(result.size(), 0);
     // homework: add verification
 }
 
 TEST(test_recursive, 2) {
     std::list<std::string> result;
     possibleParenthesis(2, result);
-
+    ASSERT_EQ(result.size(), 2);
+    result.remove("{}{}");
+    ASSERT_EQ(result.size(), 1);
+    result.remove("{{}}");
+    ASSERT_EQ(result.size(), 0);
     // homework: add verification
 }
 
 TEST(test_recursive, 3) {
     std::list<std::string> result;
     possibleParenthesis(3, result);
-
+    std::string results[5] = {"{}{}{}", "{}{{}}", "{{}}{}", "{{}{}}", "{{{}}}"};
+    for (int i = 5; i >= 0; i--) {
+        result.remove(results[i]);
+        ASSERT_EQ(result.size(), i);
+    }
+//    ASSERT_EQ(result.size(), 5);
+//    result.remove("{}{}{}");
+//    ASSERT_EQ(result.size(), 4);
+//    result.remove("{}{{}}");
+//    ASSERT_EQ(result.size(), 3);
+//    result.remove("{{}}{}");
+//    ASSERT_EQ(result.size(), 2);
+//    result.remove("{{}{}}");
+//    ASSERT_EQ(result.size(), 1);
+//    result.remove("{{{}}}");
+//    ASSERT_EQ(result.size(), 0);
     // homework: add verification
 }
 
