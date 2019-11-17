c5a00c8d9de6475d6b1e5efe2af8b6e4ac259315
Nimzerz <aminaimi14@gmail.com>
2019-11-16 20:19:31 -0800

    final homework commit

--- possible_parentheses/src/possible_parentheses.h
+++ possible_parentheses/src/possible_parentheses.h
@@ -2,16 +2,21 @@
 
 #define MAX_SIZE 100
 
+// Used class example as a reference to write this function
 void possibleParenthesis(int pos, int n, int open, int closed, std::list<std::string> & result) {
-    static char str[MAX_SIZE];
+    static char str[MAX_SIZE]; // stores a combination of open and closed brackets
     if (closed == n) {
-        result.push_back(str);
+        result.push_back(str); // store the finished result
         return;
     }
+
+    // must be n number of open brackets
     if (open < n) {
         str[pos] = '{';
         possibleParenthesis(pos + 1, n, open + 1, closed, result);
     }
+
+    // number of open brackets must match number of closed brackets
     if (open > closed) {
         str[pos] = '}';
         possibleParenthesis(pos + 1, n, open, closed + 1, result);
